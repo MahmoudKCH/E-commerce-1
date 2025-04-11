0a0f89888dd194cda2ff1167d40cc747a2b7ddd2
greedoor <firas.mhiri@ieee.org>
2025-04-11 18:34:47 +0100

    commit

--- products.php
+++ products.php
@@ -202,14 +202,7 @@
     </head>
     <body>
         <nav>
-<<<<<<< Updated upstream
             
-=======
-            <div id="navbar">
-                <div id="navbar-logo">
-                    <img src="images\AppLOGO1.png" width="110" height="90">
-                </div>
->>>>>>> Stashed changes
                 <div id="menu-bar">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
