802042fc222c31c8a47df663b8c7df3c8a30c12c
greedoor <firas.mhiri@ieee.org>
2025-04-11 18:25:44 +0100

    commit
    11/04/25//18.25

--- products.php
+++ products.php
@@ -202,101 +202,40 @@
     </head>
     <body>
         <nav>
-            <div id="navbar">
-                <div id="navbar-logo">
-                    <img src="images/eLife.png" alt="eLife, Company Ecommerce Logo">
-                </div>
+            
                 <div id="menu-bar">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
                     </svg>
                 </div>
+            <div id="navbar">
+                <div id="navbar-logo">
+                    <img src="images\AppLOGO1.png" width="110" height="90">
+                </div>
                 <div id="navbar-links">
                     <ul>
                         <li>
-                            <a href="index.php">
-                                Home
-                            </a>
+                            <a href="index.php">Home</a>
                         </li>
                         <li>
-                            <a href="products.php">
-                                Products
-                            </a>
+                            <a href="products.php">Products</a>
                         </li>
                         <li>
-                            <a href="#">
-                                Services
-                            </a>
+                            <a href="#">Services</a>
                         </li>
                         <li>
-                            <a href="#">
-                                About
-                            </a>
+                            <a href="#">About</a>
                         </li>
                     </ul>
-                    <div class="collection-tools" id="navbar-tools">
-                        <a class="cart-link" href="#">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
-                                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
-                            </svg>
-                            <span>0</span>
-                        </a>
-                        <a class="wishlist-link" href="#">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-                                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
-                            </svg>
-                            <span>0</span>
-                        </a>
-                    </div>
+                    <button class="blue-button">
+                        <a href="shp-now.php">Account</a>
+                    </button>
                 </div>
             </div>
-        </nav>
+        
+                  
 
-        <!-- MENU BAR PAGE -->
-        <div id="menu-page">
-            <div id="menu-page-wrapper">
-                <ul>
-                    <li>
-                        <a href="index.php">
-                            Home
-                        </a>
-                    </li>
-                    <li>
-                        <a href="products.php">
-                            Products
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Services
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            About
-                        </a>
-                    </li>
-                    <li class="collection-tools" id="menu-tools">
-                        <a class="cart-link" href="#">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
-                                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
-                            </svg>
-                            <span>0</span>
-                        </a>
-                        <a class="wishlist-link" href="#">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-                                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
-                            </svg>
-                            <span>0</span>
-                        </a>
-                    </li>
-                </ul>
-
-                <button class="blue-button">
-                    Shop Now
-                </button>
-            </div>
-        </div>  
+        </nav>   
         <!-- END OF MENU PAGE --> 
 
         <!-- MAIN BODY -->
@@ -1663,10 +1602,10 @@
         <div id="footer-bottom">
             <ul>
                 <li>
-                    <a href="index.php">Home</a>
+                    <a href="index.html">Home</a>
                 </li>
                 <li>
-                    <a href="products.php">Products</a>
+                    <a href="products.html">Products</a>
                 </li>
                 <li>
                     <a href="#">Services</a>
